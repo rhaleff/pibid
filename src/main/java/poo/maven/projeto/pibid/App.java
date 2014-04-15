976662016fd3e68c88433c4823e09e7cbebaf07a
marinaqueiroz <marina.oliveira@dce.ufpb.br>
2014-04-15 12:33:46 -0300

    consertando

---
+++ src/main/java/poo/maven/projeto/pibid/App.java
@@ -0,0 +1,13 @@
+package poo.maven.projeto.pibid;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
